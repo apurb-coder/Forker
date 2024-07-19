c8eee6085bdce971ec712fbf700878ac9d2cb887
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-20 00:14:55 +0530

    HeroSection Basic Setup

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -2,8 +2,51 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
+    <div className="flex">
+      {/* box-1 */}
+      <a href="">
+        <div>
+          <img src="" alt="" />
+        </div>
+        {/* author */}
+        <div></div>
+        {/* Title */}
+        <div></div>
+        {/* Description */}
+        <p></p>
+        <div className="text-[#fd7a33] underline">Read More...</div>
+      </a>
+      {/* Side Box */}
+      <div>
+        {/* box-2 */}
+        <a href="">
+          <div>
+            <img src="" alt="" />
+          </div>
+          {/* author */}
+          <div></div>
+          {/* Title */}
+          <div></div>
+          {/* Description */}
+          <p></p>
+          <div className="text-[#fd7a33] underline">Read More...</div>
+        </a>
+        {/* box-3 */}
+        <a href="">
+          <div>
+            <img src="" alt="" />
+          </div>
+          {/* author */}
+          <div></div>
+          {/* Title */}
+          <div></div>
+          {/* Description */}
+          <p></p>
+          <div className="text-[#fd7a33] underline">Read More...</div>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection
--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -5,13 +5,14 @@ const Blogs = () => {
     <div className=" flex justify-center items-center">
       <div className="mt-16 max-w-[947px] flex justify-center items-center ">
         <div className="w-[834px] text-center font-semibold">
-          <div className="text-[2rem] font-semibold text-[#3d3d3d]">
+          <div className="text-[2rem] font-semibold text-[#3d3d3d] font-size:clamp(24px,5vw,48px)">
             <span className="text-[#F76f32]">FROKER </span>BLOG
           </div>
           <p className="text-[3rem] text-[#3d3d3d]">
             Articles covering the most recent updates and advancements
           </p>
         </div>
+        {/* TODO: HEro Section */}
         {/* TODO: Recent Post */}
       </div>
     </div>
