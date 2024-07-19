d8c8a881fc13897b6b3d54a106ad6f10588ba52f
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-19 17:45:03 +0530

    .

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Blogs = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center text-[#F76f32] text-xl font-semibold">
-      Blogs
+    <div className=" flex justify-center items-center">
+      <div className='mt-16 max-w-[947px]'>
+        <div className="w-[834px] text-center m-16 font-semibold">
+          <div className="text-[2rem] font-semibold text-[#3d3d3d]">
+            <span className="text-[#F76f32]">FROKER </span>BLOG
+          </div>
+          <p className="text-[3rem] text-[#3d3d3d]">
+            Articles covering the most recent updates and advancements
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
