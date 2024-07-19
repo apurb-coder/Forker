15225120a9224ce620e9f8fa2b2df7ec50898c04
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-20 03:01:53 +0530

    pagination

--- client/src/components/RecentPost.jsx
+++ client/src/components/RecentPost.jsx
@@ -9,10 +9,11 @@ const RecentPost = () => {
       <div className="text-3xl font-semibold text-[#3d3d3d]">Recent Posts</div>
       <div className="grid grid-rows-3 pt-[0.5rem]"></div>
       {/* Pagination buttons */}
-      <div className="mt-[3rem] space-x-2">
+      <div className="mt-[3rem] space-x-2 flex justify-center">
         <button className="border-[1px] border-gray-400 p-1 text-gray-600 rounded-full font-bold">
           <NavigateBeforeIcon />
         </button>
+        {/* TODO: Generate Number of pages through API calls, if current page==1 disable left arrow, if current page==last disable right arrow */}
         <button className="border-[1px] border-gray-400 px-3 py-1 text-gray-600 rounded-full">
           1
         </button>
