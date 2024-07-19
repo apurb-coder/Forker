c4a885582b481e98fa20772081c986d0eba65009
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-20 00:33:47 +0530

    .

--- client/src/components/RecentPost.jsx
+++ client/src/components/RecentPost.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const RecentPost = () => {
+  
   return (
-    <div>RecentPost</div>
+    <div className='flex flex-col'>
+      <div className='text-2xl font-semibold relative'>Recent Posts</div>
+      <div
+      className='grid grid-rows-3 pt-[0.5rem]'></div>
+      {/* Pagination buttons */}
+      <div className='mt-[3rem]'></div>
+    </div>
   )
 }
 
--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import HeroSection from "../components/HeroSection.jsx"
+import RecentPost from '../components/RecentPost';
 
 const Blogs = () => {
   return (
     <div className=" flex justify-center items-center">
-      <div className="mt-16 max-w-[947px] flex justify-center items-center ">
+      <div className="mt-16 max-w-[947px] flex flex-col  ">
         <div className="w-[834px] text-center font-semibold">
           <div className="text-[2rem] font-semibold text-[#3d3d3d] font-size:clamp(24px,5vw,48px)">
             <span className="text-[#F76f32]">FROKER </span>BLOG
@@ -12,8 +14,10 @@ const Blogs = () => {
             Articles covering the most recent updates and advancements
           </p>
         </div>
-        {/* TODO: HEro Section */}
+        {/* TODO: Hero Section */}
+        {/* <HeroSection/> */}
         {/* TODO: Recent Post */}
+        <RecentPost />
       </div>
     </div>
   );
