61b0099f722bee3dbffb71ca402d69d3e71602f6
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-19 19:55:21 +0530

    .

--- client/src/components/UpperFooter.jsx
+++ client/src/components/UpperFooter.jsx
@@ -8,12 +8,13 @@ import LinkedInLogo from "../assets/linkedin.svg";
 import FaceBookLogo from "../assets/facebook.svg";
 import InstaGramLogo from "../assets/instagram.svg";
 import YoutubeLogo from "../assets/youtube.svg";
+import QrCode from "../assets/froker-1-3-10_pb9yln.jpg"
 
 const UpperFooter = () => {
   return (
     <div>
       <div>{/* TODO: NewsLetter */}</div>
-      <div className="flex justify-around items-center mb-16">
+      <div className="flex justify-around mb-16">
         <img src={FrokerLogo} alt="frokerlogo" className="w-[168px] h-[82px]" />
         <div className="space-y-3">
           <h4 className="text-[#F76f32] font-semibold text-2xl">Quicklink</h4>
@@ -53,11 +54,18 @@ const UpperFooter = () => {
         <div className="space-y-3">
           <h4 className="text-[#F76f32] font-semibold text-2xl">Contacts</h4>
           <div className="flex space-x-3">
-            <img src={LocationIcon} alt="locationicon" />
+            <a
+              target="_blank"
+              href="https://www.google.com/maps/place/Froker/@12.9556403,77.7202612,15z/data=!4m6!3m5!1s0x3bae13d152fd68ed:0x7427946171c830c1!8m2!3d12.9556403!4d77.7202612!16s%2Fg%2F11stskm2hv?entry=ttu"
+            >
+              <img src={LocationIcon} alt="locationicon" />
+            </a>
             <div>Whitefield, Bengaluru, 560066</div>
           </div>
           <div className="flex space-x-3">
-            <img src={MailIcon} alt="locationicon" />
+            <a href="mailto:support@froker.in">
+              <img src={MailIcon} alt="locationicon" />
+            </a>
             <div>support@froker.in</div>
           </div>
           <div className="flex space-x-3">
@@ -81,10 +89,11 @@ const UpperFooter = () => {
             </a>
           </div>
         </div>
-        <div>
+        <div className="space-y-3">
           <h4 className="text-[#F76f32] font-semibold text-2xl">
             Scan To Download
           </h4>
+          <img src={QrCode} alt="qrcode" />
         </div>
       </div>
     </div>
