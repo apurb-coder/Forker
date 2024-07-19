8d54513dc7bf08528d5e49375328b08defdc4ad1
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-19 17:05:00 +0530

    .

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import FrokerLogo from '../assets/frokerLogo.png'
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center px-16 py-3">
+      <img src={FrokerLogo} alt="logo" className="w-[7.1875rem] h-[3.5rem]" />
+      <ul className='flex space-x-16 text-[#F76f32] text-[19.2px] font-semibold'>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/blogs">Blogs</a>
+        </li>
+        <li>
+          <a href="/froker">DiscoverFroker</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar
---
+++ client/src/pages/Blogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center text-[#F76f32] text-xl font-semibold">
+      Blogs
+    </div>
+  );
+}
+
+export default Blogs
