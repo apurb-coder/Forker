8146316abc74264b54cd1e7ca8bf3f9caec22ff2
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-19 22:36:31 +0530

    fixed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import footerVector from "../assets/base.3f5339217a654a163327574449f303a4.svg"
+import React from "react";
+import footerVector from "../assets/base.3f5339217a654a163327574449f303a4.svg";
 
 const Footer = () => {
   return (
     <footer className="mt-auto">
-      <div
-        className={`url(../assets/base.3f5339217a654a163327574449f303a4.svg)`}
-      >
-        © 2024 Arroz Technology. All rights reserved
+      <div className="relative">
+        <img src={footerVector} alt="footer" />
+        <div className=" absolute bottom-3 text-center">© 2024 Arroz Technology. All rights reserved</div>
       </div>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
