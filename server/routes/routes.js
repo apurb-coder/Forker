4f467701c11e0777d3ed255984b8d8eb61f52a19
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-20 17:20:51 +0530

    /blogs/pagination implementation

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -105,4 +105,52 @@ router.get("/blog-page", async (req, res) => {
   }
 });
 
+// from client side send with a ?page=number&limit=9
+router.get("/blogs/pagination", async (req, res) => {
+  try {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 1; // generally ask for 9 limit
+
+    const fieldsToReturn = {
+      blogNumber: 1,
+      like: 1,
+      createdAt: 1,
+      authorName: 1,
+      title: 1,
+      content: 1,
+      titleImage: 1,
+    };
+
+    const totalPosts = await BlogPost.countDocuments();
+    const totalPages = Math.ceil(totalPosts / limit);
+
+    if (page < 1 || page > totalPages) {
+      return res.status(400).json({
+        success: false,
+        error: "Invalid page parameter",
+      });
+    }
+
+    const skip = (page - 1) * limit;
+    // const endIndex = page * limit;
+
+    const blogPosts = await BlogPost.find({}, fieldsToReturn)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({
+      success: true,
+      data: blogPosts,
+      currentPage: page,
+      totalPages,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Error fetching blog posts",
+    });
+  }
+});
+
 export { router };
