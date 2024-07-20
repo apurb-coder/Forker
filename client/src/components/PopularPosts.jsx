2a2d7b1ba0ffe8e9762269d33a9e52431b55293f
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-21 04:40:16 +0530

    Pagination of PopularPost.jsx is fixed

--- client/src/components/PopularPosts.jsx
+++ client/src/components/PopularPosts.jsx
@@ -23,7 +23,7 @@ const PopularPosts = () => {
       const response = await axios.get(
         `${
           import.meta.env.VITE_BACKEND_URL
-        }/popular-posts?page=${pageNumber}&limit=9`
+        }/blogs/pagination?page=${pageNumber}&limit=9`
       );
       setPopularPosts(response.data.data);
       setTotalPages(response.data.totalPages);
@@ -51,32 +51,31 @@ const PopularPosts = () => {
 
   return (
     <div className="flex flex-col mt-14">
-      <div className="text-3xl font-semibold text-[#3d3d3d]">Popular Posts</div>
+      <div className="text-3xl font-semibold text-[#3d3d3d]">Recent Posts</div>
       <div className="grid grid-rows-3 grid-flow-col gap-4 pt-10">
         {popularPosts.map((post) => (
-          <div key={post._id}>
-            <div
-              className="boox hover:cursor-pointer"
-              onClick={() => handleClickPost(post.BlogNumber)}
-            >
-              <div className="imagee">
-                <img
-                  src={post.titleImage}
-                  alt="title Image"
-                  className="rounded-[2rem]"
-                />
-              </div>
-              <div className="author text-[#fd7a33] font-bold pt-3">
-                by {post.authorName} -{" "}
-                {moment(post.createdAt).format("D MMMM YYYY")}
-              </div>
-              <div className="ins mb-2 font-semibold text-[#1a1a1a]">
-                {post.title}
-              </div>
-              <p className="text-[#667085]">{post.description}</p>
-              <div className="text-[#fd7a33] underline font-semibold">
-                Read More...
-              </div>
+          <div
+            key={post._id}
+            className="boox hover:cursor-pointer"
+            onClick={() => handleClickPost(post.BlogNumber)}
+          >
+            <div className="imagee">
+              <img
+                src={post.titleImage}
+                alt="title Image"
+                className="rounded-[2rem]"
+              />
+            </div>
+            <div className="author text-[#fd7a33] font-bold pt-3">
+              by {post.authorName} -{" "}
+              {moment(post.createdAt).format("D MMMM YYYY")}
+            </div>
+            <div className="ins mb-2 font-semibold text-[#1a1a1a]">
+              {post.title}
+            </div>
+            <p className="text-[#667085]">{post.description}</p>
+            <div className="text-[#fd7a33] underline font-semibold">
+              Read More...
             </div>
           </div>
         ))}
