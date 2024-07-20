aa2e47875a2a9fb811f11502e0881cdef1c64775
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-20 12:36:54 +0530

    article image

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -13,21 +13,26 @@ const BlogPage = () => {
         <div>Blog Name</div>
       </div>
       {/* articls image */}
-      <div className="">
+      <div className="relative rounded-[2rem]">
         <img
           src="https://res.cloudinary.com/dapbrn8a9/image/upload/q_auto:low/v1714712971/Screenshot_2024-05-03_103708_dx4yto.png"
           alt="article Img"
           className="rounded-[2rem]"
         />
+        <div className="absolute top-0 left-0 w-full h-full bg-black/50 rounded-[2rem]"></div>
+        <div className="absolute bottom-[4%] left-[2%] uppercase text-white font-bold text-2xl">
+          {/* Title over the Article Image */}
+          Lorem ipsum dolor sit
+        </div>
       </div>
       {/* Author and details */}
-      <div className='flex justify-between my-3'>
+      <div className="flex justify-between my-3">
         <div className="text-[#F76f32]">Name of Author</div>
-        <div className='flex items-center space-x-4'>
+        <div className="flex items-center space-x-4">
           <img
             src="https://res.cloudinary.com/dapbrn8a9/image/upload/q_auto:low/v1707285106/Frokerassets/Artboard_ne0yo2.jpg"
             alt="like icon"
-            className='w-9 h-9 text-orange-500'
+            className="w-9 h-9 text-orange-500"
           />
           <div className="">1 Likes</div>
         </div>
