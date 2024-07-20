ab764155a2ce040c57ae6b33362bf891b585c067
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-20 11:02:47 +0530

    navBar hover animation

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -7,14 +7,14 @@ const NavBar = () => {
       <img src={FrokerLogo} alt="logo" className="w-[7.1875rem] h-[3.5rem]" />
       <ul className="flex space-x-16 text-[#F76f32] text-[19.2px] font-semibold">
         <div>
-          <li className="py-2 px-4 border-b-[3px] border-white hover:border-[#F76f32] cursor-pointer">
+          <li className="py-2 px-4 border-b-[3px] border-white hover:border-[#F76f32] hover:transition-all hover:duration-700 hover:ease-in-out cursor-pointer">
             <a href="/">Home</a>
           </li>
         </div>
-        <li className="py-2 px-4 border-b-[3px] border-white hover:border-[#F76f32] cursor-pointer">
+        <li className="py-2 px-4 border-b-[3px] border-white hover:border-[#F76f32] hover:transition-all hover:duration-700 hover:ease-in-out cursor-pointer">
           <a href="/blogs">Blogs</a>
         </li>
-        <li className="py-2 px-4 border-b-[3px] border-white hover:border-[#F76f32] cursor-pointer">
+        <li className="py-2 px-4 border-b-[3px] border-white hover:border-[#F76f32] hover:transition-all hover:duration-700 hover:ease-in-out cursor-pointer">
           <a href="/froker">DiscoverFroker</a>
         </li>
       </ul>
